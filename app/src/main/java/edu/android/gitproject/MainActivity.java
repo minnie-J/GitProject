85d66b47cd5078c82c9f31df9c4df062ae9d148b
Joominseok <alstjr@naver.com>
2018-04-16 16:50:15 +0900

    4시50분 테스트

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("안녕~!!!!!");
         textView.setText("어서오세요!!");
         textView.setText("놀러와요");
+        textView.setText("테스트");
     }
 }
